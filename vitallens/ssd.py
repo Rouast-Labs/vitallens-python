7262fde8e30f848476881684046d634945d8f646
Philipp Rouast <philipp@rouast.com>
2024-05-21 08:41:42 +0200

    Attempt to fix issue with importlib for different Python versions

--- vitallens/ssd.py
+++ vitallens/ssd.py
@@ -26,7 +26,7 @@ from prpy.numpy.signal import interpolate_vals
 import sys
 from typing import Tuple
 
-if sys.version_info >= (3.9):
+if sys.version_info >= (3, 9):
   from importlib.resources import files
 else:
   from importlib_resources import files
