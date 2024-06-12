2e45e1c8e13784765e3f4398cadb89aee388800e
Philipp Rouast <philipp@rouast.com>
2024-06-12 18:00:31 +0200

    Add samples

--- examples/test.py
+++ examples/test.py
@@ -1,35 +1,80 @@
 import sys
 sys.path.append('../vitallens-python')
-import vitallens
-import timeit
-
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
 from prpy.ffmpeg.probe import probe_video
 from prpy.ffmpeg.readwrite import read_video_from_path
+from prpy.helpers import str2bool
+from prpy.numpy.signal import estimate_freq
+import timeit
+from vitallens import VitalLens, Method
 
-input_str = True
-# TODO: Select a better test video
-video_path = "examples/test.mp4"
+COLOR_GT = '#000000'
+METHOD_COLORS = {
+  Method.VITALLENS: '#00a4df',
+  Method.G: '#00ff00',
+  Method.CHROM: '#4ceaff',
+  Method.POS: '#23b031'
+}
 
-if input_str:
-  fps = None
-  video = video_path
-  print("Using video at: {}".format(video_path))
-else:
-  fps, *_ = probe_video(video_path)
-  print("Reading full video from {}...".format(video_path))
-  video, _ = read_video_from_path(path=video_path, pix_fmt='rgb24')
-  print("Video shape: {}".format(video.shape))
+def run(args=None):
+  # Get ground truth vitals
+  vitals = pd.read_csv(args.vitals_path)
+  ppg_gt = vitals['ppg'] if 'ppg' in vitals else None 
+  resp_gt = vitals['resp'] if 'resp' in vitals else None
+  # Get video
+  fps, *_ = probe_video(args.video_path)
+  if args.input_str:
+    video = args.video_path
+    print("Using video at: {}".format(args.video_path))
+  else:
+    print("Reading full video into memory from {}...".format(args.video_path))
+    video, _ = read_video_from_path(path=args.video_path, pix_fmt='rgb24')
+    print("Video shape: {}".format(video.shape))
+  # Estimate vitals and measure inference time
+  vl = VitalLens(
+    method=args.method,
+    api_key="YOUR_API_KEY")
+  start = timeit.default_timer()
+  result = vl(video=video, fps=fps)
+  stop = timeit.default_timer()
+  print("Inference time: {:.2f} ms".format((stop-start)*1000))
+  # Plot the results
+  if 'resp' in result[0]:
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+  else:
+    fig, ax1 = plt.subplots(1)
+  fig.suptitle('Vital signs estimated from {} using {}'.format(args.video_path, args.method.name))
+  if "pulse" in result[0] and ppg_gt is not None:
+    hr_gt = estimate_freq(ppg_gt, f_s=fps, f_res=0.005, f_range=(40./60., 240./60.), method='periodogram') * 60.
+    ax1.plot(ppg_gt, color=COLOR_GT, label='Pulse Ground Truth -> HR: {:.1f} bpm'.format(hr_gt))
+  if "resp" in result[0] and resp_gt is not None:
+    rr_label = estimate_freq(resp_gt, f_s=fps, f_res=0.005, f_range=(4./60., 90./60.), method='periodogram') * 60.
+    ax2.plot(resp_gt, color=COLOR_GT, label='Resp Ground Truth -> RR: {:.1f} bpm'.format(rr_label))
+  if "pulse" in result[0]:
+    hr = estimate_freq(result[0]['pulse']['val'], f_s=fps, f_res=0.005, f_range=(40./60., 240./60.), method='periodogram') * 60.
+    ax1.plot(result[0]['pulse']['val'], color=METHOD_COLORS[args.method], label='Pulse Estimate -> HR: {:.1f} bpm'.format(hr))
+    ax1.plot(result[0]['pulse']['conf'], color=METHOD_COLORS[args.method], label='Pulse Estimation Confidence')
+  if "resp" in result[0]:
+    rr = estimate_freq(result[0]['resp']['val'], f_s=fps, f_res=0.005, f_range=(4./60., 90./60.), method='periodogram') * 60.
+    ax2.plot(result[0]['resp']['val'], color=METHOD_COLORS[args.method], label='Resp Estimate -> RR: {:.1f} bpm'.format(rr))
+    ax2.plot(result[0]['resp']['conf'], color=METHOD_COLORS[args.method], label='Resp Estimation Confidence')
+  ax1.legend()
+  if 'resp' in result[0]: ax2.legend()
+  plt.show()
 
-vl = vitallens.VitalLens(
-  method=vitallens.Method.VITALLENS,
-  api_key="INSERT_API_KEY_HERE")
-start = timeit.default_timer()
-result = vl(video=video, fps=fps, override_fps_target=30.0)
-stop = timeit.default_timer()
+def method_type(name):
+  try:
+    return Method[name]
+  except KeyError:
+    raise argparse.ArgumentTypeError(f"{name} is not a valid Method")
 
-print("Inference time: {:.2f} ms".format((stop-start)*1000))
-
-import matplotlib.pyplot as plt
-if 'pulse' in result[0]: plt.plot(result[0]['pulse']['val'])
-if 'resp' in result[0]: plt.plot(result[0]['resp']['val'])
-plt.show()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--vitals_path', type=str, default='examples/sample_vitals_1.csv', help='Path to ground truth vitals')
+  parser.add_argument('--video_path', type=str, default='examples/sample_video_1.mp4', help='Path to video')
+  parser.add_argument('--method', type=method_type, default='POS', help='Choice of method', required=True)
+  parser.add_argument('--input_str', type=str2bool, default=True, help='If true, pass filepath to VitalLens, otherwise read video into memory first')
+  args = parser.parse_args()
+  run(args)
