e5e0c4024e0ef45b65d43de8614362b28c8f1927
Philipp Rouast <philipp@rouast.com>
2025-08-31 21:52:48 +1000

    Finish tests

--- tests/test_vitallens.py
+++ tests/test_vitallens.py
@@ -31,7 +31,7 @@ import sys
 sys.path.append('../vitallens-python')
 
 from vitallens.constants import API_MAX_FRAMES, API_MIN_FRAMES, API_URL
-from vitallens.enums import Mode
+from vitallens.enums import Method, Mode
 from vitallens.methods.vitallens import VitalLensRPPGMethod
 from vitallens.utils import load_config
 
@@ -71,6 +71,7 @@ def create_mock_api_response(
   if api_key is None or not isinstance(api_key, str) or len(api_key) < 30:
     return create_mock_response(
       status_code=403, json_data={"vital_signs": None, "face": None, "state": None, "message": "Error"}) 
+  model = json.get("model", "vitallens-1.0")
   video_base64 = json["video"]
   if video_base64 is None:
     return create_mock_response(
@@ -83,48 +84,65 @@ def create_mock_api_response(
   if video.shape[0] < API_MIN_FRAMES or video.shape[0] > API_MAX_FRAMES:
     return create_mock_response(status_code=400, json_data={"vital_signs": None, "face": None, "state": None, "message": "Error"})
   return create_mock_response(
-      status_code=200,
-      json_data={
-        "vital_signs": {
-          "heart_rate": {"value": 60.0, "unit": "bpm", "confidence": 0.99, "note": "Note"},
-          "respiratory_rate": {"value": 15.0, "unit": "bpm", "confidence": 0.97, "note": "Note"},
-          "ppg_waveform": {"data": np.random.rand(video.shape[0]).tolist(), "unit": "unitless", "confidence": np.ones(video.shape[0]).tolist(), "note": "Note"},
-          "respiratory_waveform": {"data": np.random.rand(video.shape[0]).tolist(), "unit": "unitless", "confidence": np.ones(video.shape[0]).tolist(), "note": "Note"}},
-        "face": {"confidence": np.random.rand(video.shape[0]).tolist(), "note": "Note"},
-        "state": {"data": np.zeros((256,), dtype=np.float32).tolist(), "note": "Note"},
-        "message": "Message"})
+    status_code=200,
+    json_data={
+      "vital_signs": {
+        "heart_rate": {"value": 60.0, "unit": "bpm", "confidence": 0.99, "note": "Note"},
+        "respiratory_rate": {"value": 15.0, "unit": "bpm", "confidence": 0.97, "note": "Note"},
+        "ppg_waveform": {"data": np.random.rand(video.shape[0]).tolist(), "unit": "unitless", "confidence": np.ones(video.shape[0]).tolist(), "note": "Note"},
+        "respiratory_waveform": {"data": np.random.rand(video.shape[0]).tolist(), "unit": "unitless", "confidence": np.ones(video.shape[0]).tolist(), "note": "Note"}},
+      "face": {"confidence": np.random.rand(video.shape[0]).tolist(), "note": "Note"},
+      "state": {"data": np.zeros((256,), dtype=np.float32).tolist(), "note": "Note"},
+      "model_used": model,
+      "message": "Message"})
 
-@pytest.mark.parametrize("file", [True, False])
 @pytest.mark.parametrize("override_fps_target", [None, 15, 10])
-@pytest.mark.parametrize("long", [False, True])
 @pytest.mark.parametrize("override_global_parse", [False, True])
+@pytest.mark.parametrize("requested_model", [Method.VITALLENS, Method.VITALLENS_1_0, Method.VITALLENS_2_0])
 @patch('requests.post', side_effect=create_mock_api_response)
-def test_VitalLensRPPGMethod_mock(mock_post, request, file, long, override_fps_target, override_global_parse):
-  if long and file:
-    pytest.skip("Skip because parameter combination does not work")
-  config = load_config("vitallens.yaml")
+def test_VitalLensRPPGMethod_file_mock(mock_post, request, override_fps_target, override_global_parse, requested_model):
   api_key = request.getfixturevalue('test_dev_api_key')
   test_video_path = request.getfixturevalue('test_video_path')
   test_video_ndarray = request.getfixturevalue('test_video_ndarray')
+  test_video_faces = request.getfixturevalue('test_video_faces')
+  method = VitalLensRPPGMethod(mode=Mode.BATCH,
+                               api_key=api_key,
+                               requested_model=requested_model)
+  data, unit, conf, note, live = method(
+    inputs=test_video_path, faces=test_video_faces,
+    override_fps_target=override_fps_target,
+    override_global_parse=override_global_parse)
+  assert all(key in data for key in method.signals)
+  assert all(key in unit for key in method.signals)
+  assert all(key in conf for key in method.signals)
+  assert all(key in note for key in method.signals)
+  assert data['ppg_waveform'].shape == (test_video_ndarray.shape[0],)
+  assert conf['ppg_waveform'].shape == (test_video_ndarray.shape[0],)
+  assert data['respiratory_waveform'].shape == (test_video_ndarray.shape[0],)
+  assert conf['respiratory_waveform'].shape == (test_video_ndarray.shape[0],)
+  assert live.shape == (test_video_ndarray.shape[0],)
+
+@pytest.mark.parametrize("override_fps_target", [None, 15, 10])
+@pytest.mark.parametrize("long", [False, True])
+@pytest.mark.parametrize("override_global_parse", [False, True])
+@pytest.mark.parametrize("requested_model", [Method.VITALLENS, Method.VITALLENS_1_0, Method.VITALLENS_2_0])
+@patch('requests.post', side_effect=create_mock_api_response)
+def test_VitalLensRPPGMethod_mock(mock_post, request, long, override_fps_target, override_global_parse, requested_model):
+  api_key = request.getfixturevalue('test_dev_api_key')
+  test_video_ndarray = request.getfixturevalue('test_video_ndarray')
   test_video_fps = request.getfixturevalue('test_video_fps')
   test_video_faces = request.getfixturevalue('test_video_faces')
-  method = VitalLensRPPGMethod(config=config,
-                               mode=Mode.BATCH,
-                               api_key=api_key)
-  if file:
-    data, unit, conf, note, live = method(
-      inputs=test_video_path, faces=test_video_faces,
-      override_fps_target=override_fps_target,
-      override_global_parse=override_global_parse)
-  else:
-    if long:
-      n_repeats = (API_MAX_FRAMES * 3) // test_video_ndarray.shape[0] + 1
-      test_video_ndarray = np.repeat(test_video_ndarray, repeats=n_repeats, axis=0)
-      test_video_faces = np.repeat(test_video_faces, repeats=n_repeats, axis=0)
-    data, unit, conf, note, live = method(
-      inputs=test_video_ndarray, faces=test_video_faces,
-      fps=test_video_fps, override_fps_target=override_fps_target,
-      override_global_parse=override_global_parse)
+  method = VitalLensRPPGMethod(mode=Mode.BATCH,
+                               api_key=api_key,
+                               requested_model=requested_model)
+  if long:
+    n_repeats = (API_MAX_FRAMES * 3) // test_video_ndarray.shape[0] + 1
+    test_video_ndarray = np.repeat(test_video_ndarray, repeats=n_repeats, axis=0)
+    test_video_faces = np.repeat(test_video_faces, repeats=n_repeats, axis=0)
+  data, unit, conf, note, live = method(
+    inputs=test_video_ndarray, faces=test_video_faces,
+    fps=test_video_fps, override_fps_target=override_fps_target,
+    override_global_parse=override_global_parse)
   assert all(key in data for key in method.signals)
   assert all(key in unit for key in method.signals)
   assert all(key in conf for key in method.signals)
@@ -138,7 +156,7 @@ def test_VitalLensRPPGMethod_mock(mock_post, request, file, long, override_fps_t
 @pytest.mark.parametrize("process_signals", [True, False])
 @pytest.mark.parametrize("n_frames", [16, 250])
 def test_VitalLens_API_valid_response(request, process_signals, n_frames):
-  config = load_config("vitallens.yaml")
+  config = load_config("vitallens-1.0.yaml")
   api_key = request.getfixturevalue('test_dev_api_key')
   test_video_ndarray = request.getfixturevalue('test_video_ndarray')
   test_video_fps = request.getfixturevalue('test_video_fps')
@@ -174,7 +192,7 @@ def test_VitalLens_API_valid_response(request, process_signals, n_frames):
   assert state.shape == (256,)
 
 def test_VitalLens_API_wrong_api_key(request):
-  config = load_config("vitallens.yaml")
+  config = load_config("vitallens-1.0.yaml")
   test_video_ndarray = request.getfixturevalue('test_video_ndarray')
   test_video_fps = request.getfixturevalue('test_video_fps')
   test_video_faces = request.getfixturevalue('test_video_faces')
